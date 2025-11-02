7ef456aadee5ad77f272340831140863581865c6
Reffel01 <adujier@gmail.com>
2025-11-02 14:03:11 +0100

    Assignment 3

---
+++ DataType.java
@@ -0,0 +1,30 @@
+public class DataType{
+    public static void main(String[] args){
+		//using byte
+		byte myAge = 25;
+		
+		//using short...
+		short quantity = 25000;
+		
+		//using int
+		int nigeriapopulation = 20000000;
+		
+		//using long
+		long worldpopulation = 9000000000;
+		
+		//using float
+		float price = 458.37F;
+		
+		//using double
+		double balance = 
+		
+		System.out.printf("I am %d years old%n",myAge);
+		System.out.printf("I ordered %d quantity of phones%n", quantity);
+		System.out.printf("The Population of Nigeria is %n", nigeriapopulation);
+		System.out.printf("The Population of the World is %d%n", worldpopulation);
+	    System.out.printf("The price for two books is %.2f%n",price);
+		System.out.printf("My account balance is $%.2f%n");
+		
+	}	
+	
+}
---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+public class HelloWorld{
+	public static void main(String[] argrs){
+		
+		int num = 50;
+		String name = "Jack";
+		Float Deci = 9.99999f;
+		boolean isFun = true;
+		char symbol = '2';
+		
+		//using print line method
+	System.out.println(" my first java program");
+	
+	//using print method
+	System.out.println("Learning Java is fun");
+	System.out.println("The print method should be used to accept input/n");
+	
+	//using printf method
+	System.out.printf("I bought %d quantity of bread",num); 
+	System.out.printf("My name is %n",name);
+	System.out.printf("Your balance is %0.2f",Deci);
+	System.out.printf("Is java fun %b",isFun);
+	//System.out.print("Do you enjoy learning "/java/"")
+	}
+}
